7b6e2f574a93b0a1bf691b5ecc013a3b2dd1ca53
yy0587023-rgb <yy0587023@gmail.com>
2026-02-02 17:24:52 +0800

    Update Phenotypic correlation analysis.r

--- Phenotypic correlation analysis.r
+++ Phenotypic correlation analysis.r
@@ -5,14 +5,14 @@ install.packages("GGally")
 library(GGally)
 library(ggplot2)
 my_cor <- function(data, mapping, ...) {
-  ggally_cor(data, mapping, ..., digits = 2)  # 保留两位小数
+  ggally_cor(data, mapping, ..., digits = 2)  
 }
 ggpairs(data1,
         columns = 2:4,
         aes(color = group),
         upper = list(continuous = wrap("cor", size = 5)),
         lower = list(continuous = wrap("points", alpha = 0.6)),
-        diag = list(continuous = wrap("densityDiag", alpha = 0.4)))  # 设置透明度
+        diag = list(continuous = wrap("densityDiag", alpha = 0.4)))  
 #Analysis by source
 #Hebei(DC)
 setwd(dir="D:\\phenotype")
@@ -44,3 +44,4 @@ test1=rcorr(as.matrix(data1))
 head(test1)
 library("PerformanceAnalytics")
 chart.Correlation(data1,histogram=TRUE)
+
